38c2a2244477ba0731b264c05019a9f4fd718d16
jason-toups <me@jasontoups.com>
2020-03-09 12:14:36 -0700

    adds react and hello test message

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css';
+import { Segment } from 'semantic-ui-react';
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <h1>Hello Dolly</h1>,
+  // <Router>
+  //   <Segment inverted>
+  //     <App />
+  //   </Segment>
+  // </Router>,
+  document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
