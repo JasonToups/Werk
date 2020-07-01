bf838070dc046ef1978f4289ce6cc60193ef9822
jason-toups <me@jasontoups.com>
2020-06-30 21:11:59 -0700

    adds overview section to homepage

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -9,8 +9,9 @@ class Welcome extends Component {
 		return (
 			<>
 				<link
-					href='https://fonts.googleapis.com/css?family=Knewave'
-					rel='stylesheet'></link>
+					href='https://fonts.googleapis.com/css2?family=Knewave&family=Roboto:wght@300;400&display=swap'
+					rel='stylesheet'
+				/>
 				<header>
 					<section className='hero-header-text'>
 						<h1 id='ready'>ARE YOU READY TO...</h1>
@@ -18,6 +19,36 @@ class Welcome extends Component {
 						<Register />
 					</section>
 				</header>
+				<section className='overview'>
+					<div className='column join-the-queens'>
+						<h1>Join the Queens</h1>
+						<p>
+							On Werk, you can tip, follow or book your favorite drag queens.
+							<br></br>
+							<br></br>
+							Can’t get out to a drag show? Tip them from the comfort of your
+							own home.<br></br>
+							<br></br> Need a Queen for your event? Send them a booking
+							request.<br></br>
+							<br></br> Queens work hard for the money and they deserve anything
+							you can give.
+						</p>
+					</div>
+					<div className='column data'>
+						<div className='data-col'>
+							<h1>145</h1>
+							<p>Queens in the community</p>
+						</div>
+						<div className='data-col'>
+							<h1>$135k</h1>
+							<p>Tips collected</p>
+						</div>
+						<div className='data-col'>
+							<h1>79</h1>
+							<p>Gigs booked</p>
+						</div>
+					</div>
+				</section>
 			</>
 		);
 	}
