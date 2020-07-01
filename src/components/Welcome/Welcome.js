0e7f599dc708d9b3f3dff7a9c331f6c8f943630b
jason-toups <me@jasontoups.com>
2020-06-30 18:20:20 -0700

    adds welcome message and link to register

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import './Welcome.css';
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './Welcome.css';
+import Register from '../NavBar/Modal/ModalRegister';
 
 class Welcome extends Component {
 	render() {
 		return (
 			<>
+				<link
+					href='https://fonts.googleapis.com/css?family=Knewave'
+					rel='stylesheet'></link>
 				<header>
 					<section className='hero-header-text'>
-						<h1>ARE YOU READY TO...</h1>
-						<h1>WERK!</h1>
-						<button>Yaaasss!</button>
+						<h1 id='ready'>ARE YOU READY TO...</h1>
+						<h1 id='werk'>Werk?</h1>
+						<Register />
 					</section>
 				</header>
 			</>
